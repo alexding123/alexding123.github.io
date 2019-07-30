34b1b0ab55a3f5f2684176332c7bad99609ab7f7
Alexander Ding <alexding123@outlook.com>
2019-07-30 19:43:56 -0400

    Updates

---
+++ precache-manifest.c2f08187055502cd068618004b363783.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406fde8daa08461a2a607622449fb997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75f400e7aadc741c77d2",
+    "url": "/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "eb5c29ea7f09467623b7",
+    "url": "/static/css/main.027feda9.chunk.css"
+  },
+  {
+    "revision": "75f400e7aadc741c77d2",
+    "url": "/static/js/2.c43fcca1.chunk.js"
+  },
+  {
+    "revision": "eb5c29ea7f09467623b7",
+    "url": "/static/js/main.dd30a15e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5cd7d7831bdc6e24e2710490d9801bd7",
+    "url": "/static/media/certificate.5cd7d783.svg"
+  },
+  {
+    "revision": "e0823d000ae32db6cff971f4071a8e13",
+    "url": "/static/media/course.e0823d00.svg"
+  },
+  {
+    "revision": "9c65d061fbdf45b56b43cf3be9ae964a",
+    "url": "/static/media/demo.9c65d061.svg"
+  },
+  {
+    "revision": "e94196796fcd701aa3076f765520642a",
+    "url": "/static/media/email.e9419679.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "de4110af088776d1419cd0c0d618522f",
+    "url": "/static/media/headshot.de4110af.png"
+  },
+  {
+    "revision": "4a56c99732c1a3ddc1f4c3f68901ec6e",
+    "url": "/static/media/logo.4a56c997.svg"
+  },
+  {
+    "revision": "96545d893fa41387eb0f1da8cc20d914",
+    "url": "/static/media/video.96545d89.svg"
+  }
+]);
--- src/Components/Divider.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function DividerComponent() {
-    return <div className="divider"></div>
-}
